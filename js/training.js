e06eae12c385f8c6c089781ce8df71f9860f9908
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-04-26 14:36:15 +0200

    Exercice 1

--- js/training.js
+++ js/training.js
@@ -1,27 +1,28 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
-
-
+const btn = document.querySelector(".button");
+const text = document.querySelector(".text");
+btn.addEventListener("click", function (event) {
+    text.classList.toggle("visible");
+});
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
-
+const scrollValue = document.getElementById("ex2-scroll-value");
+window.addEventListener("scroll", function () {
+    scrollValue.textContent = Math.round(window.scrollY);
+});
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
 
-
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
-
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
 
-
 /* ------------------------------------ */
 /* --- Exercice 6 --- */
-
-
 
 /* ------------------------------------ */
 /* --- Exercice 7 --- */
@@ -32,9 +33,8 @@ const taskList = [
     "🐶 Sortir le chien",
     "🍽️ Faire la vaisselle",
     "🧹 Passer l'aspirateur",
-    "🌳 Tondre la pelouse"
+    "🌳 Tondre la pelouse",
 ];
-
 
 /* ------------------------------------ */
 /* --- Exercice 8 --- */
