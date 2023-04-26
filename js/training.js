d5340a335774a5d00554df4b66ba628fbf25594d
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-04-26 16:15:43 +0200

    exercice 5

--- js/training.js
+++ js/training.js
@@ -36,12 +36,16 @@ button.addEventListener("click", () => {
     //     indexColors = 0;
     // }
 });
-// button.classList.add("red");
-// button.classList.remove("red");
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
-
+const buttons = document.querySelectorAll("#ex5 .button");
+buttons.forEach((button) => {
+    button.addEventListener("mouseover", () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        button.style.backgroundColor = "#" + randomColor;
+    });
+});
 /* ------------------------------------ */
 /* --- Exercice 6 --- */
 
